1fa8b16dafa5e904323c68614ae67eadc7042368
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-03-11 22:49:10 +0200

    Add calculator extension method

---
+++ VkInterestingPostExtractor/Extensions/AddCalculatorExtension.cs
@@ -0,0 +1,17 @@
+﻿using VkInterestingPostExtractor.CalculationEngine;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class AddCalculatorExtension
+    {
+        public static IServiceCollection AddCalculator(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .AddTransient<ICalculator, Calculator>()
+                .AddTransient<DataProcessor<IDataHandler>>()
+                .AddTransient<IDataHandler, ViewsHandler>()
+                .AddTransient<IDataHandler, RepostsHandler>()
+                .AddTransient<IDataHandler, LikesHandler>();
+        }
+    }
+}
