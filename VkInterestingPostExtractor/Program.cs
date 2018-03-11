5dc56a9f42a33e00ade43a97c9bedce7d52b831c
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-03-11 22:50:06 +0200

    Modify main program class

--- VkInterestingPostExtractor/Program.cs
+++ VkInterestingPostExtractor/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
+[assembly: InternalsVisibleTo("Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 namespace VkInterestingPostExtractor
 {
     class Program
     {
+        private static IConfiguration _configuration;
+        private static IServiceProvider _serviceProvider;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateConfiguration();
+            CreateServiceProvider();
+
+            _serviceProvider.GetService<VkMostInterestingPostFetcher>().Fetch();
+        }
+
+        private static void CreateConfiguration()
+        {
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, false)
+                .Build();
+        }
+
+        private static void CreateServiceProvider()
+        {
+            _serviceProvider = new ServiceCollection()
+                .AddTransient<VkMostInterestingPostFetcher>()
+                .AddVkApiFactory(_configuration)
+                .AddSingleton<GroupInfoCollection>()
+                .AddTransient<WallPostFetcher>()
+                .AddTransient(s => new DateTimeOffsetFetcher(7))
+                .AddTransient<GroupInfoResolver>()
+                .AddTransmitters(_configuration)
+                .AddCalculator()
+                .BuildServiceProvider();
         }
     }
 }
