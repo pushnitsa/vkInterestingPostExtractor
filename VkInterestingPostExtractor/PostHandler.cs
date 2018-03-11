58efda5343dfb5df81b4e9d9c632d9ec6ea60c4c
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-03-11 22:47:51 +0200

    Add Post handler

---
+++ VkInterestingPostExtractor/PostHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VkInterestingPostExtractor.CalculationEngine;
+using VkNet.Model;
+
+namespace VkInterestingPostExtractor
+{
+    public class PostHandler
+    {
+        private readonly ICalculator _calculator;
+        private IReadOnlyCollection<Post> _postsCollection;
+        
+        public PostHandler(ICalculator calculator)
+        {
+            _calculator = calculator;
+        }
+        
+        public void Handle(int membersCount, IReadOnlyCollection<Post> posts)
+        {
+            _postsCollection = posts;
+            
+            foreach (var post in _postsCollection)
+            {
+                var repostsCount = post.Reposts.Count;
+                var likesCount = post.Likes.Count;
+                var views = post.Views.Count;
+            }
+        }
+
+        private void CalculateMedian()
+        {
+            var likesMedian = _calculator.GetLikesMedian(_postsCollection);
+            var repostsMedian = _calculator.GetRepostsMedian(_postsCollection);
+            var viewsMedian = _calculator.GetViewsMedian(_postsCollection);
+        }
+    }
+}
