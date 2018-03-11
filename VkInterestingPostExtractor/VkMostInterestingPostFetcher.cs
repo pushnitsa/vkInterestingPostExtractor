9b5dc2c7643f71ca88d483948d9dc6c6bbebde56
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-03-11 22:49:41 +0200

    Add most interesting post fetcher

---
+++ VkInterestingPostExtractor/VkMostInterestingPostFetcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using VkNet.Model;
+
+namespace VkInterestingPostExtractor
+{
+    class VkMostInterestingPostFetcher
+    {
+        private readonly GroupInfoCollection _groupInfoCollection;
+        private readonly WallPostFetcher _wallPostFetcher;
+
+        public VkMostInterestingPostFetcher(
+            GroupInfoCollection groupInfoCollection,
+            WallPostFetcher wallPostFetcher
+            )
+        {
+            _groupInfoCollection = groupInfoCollection;
+            _wallPostFetcher = wallPostFetcher;
+        }
+
+        public void Fetch()
+        {
+            var groupIds = _groupInfoCollection.GetGroupIds();
+
+            foreach (var groupId in groupIds)
+            {
+                var posts = _wallPostFetcher.FetchPosts(groupId);
+            }
+        }
+    }
+}
