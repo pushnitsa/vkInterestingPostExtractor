57345972d77d4c79fe9af053835223e715e47295
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-03-11 22:48:05 +0200

    Add group info collection

---
+++ VkInterestingPostExtractor/GroupInfoCollection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VkNet.Utils;
+
+namespace VkInterestingPostExtractor
+{
+    internal class GroupInfoCollection
+    {
+        private readonly GroupInfoResolver _resolver;
+        private IReadOnlyList<GroupInfo> _groupInfoCollection;
+        
+        public GroupInfoCollection(GroupInfoResolver resolver)
+        {
+            _resolver = resolver;
+        }
+
+        public IReadOnlyCollection<long> GetGroupIds()
+        {
+            return GetGroupInfoCollection().Select(gi => gi.GroupId).ToReadOnlyCollection();
+        }
+
+        private IReadOnlyCollection<GroupInfo> GetGroupInfoCollection()
+        {
+            return _groupInfoCollection ?? (_groupInfoCollection = _resolver.Resolve());
+        }
+    }
+}
