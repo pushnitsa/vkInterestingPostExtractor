4df726e62561b4dc1af48466bdffea16dd766ca8
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-03-11 22:48:44 +0200

    Add transmitters extension method

---
+++ VkInterestingPostExtractor/Extensions/AddTransmittersExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using VkInterestingPostExtractor;
+using VkInterestingPostExtractor.Transmitters;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    internal static class AddTransmittersExtension
+    {
+        public static IServiceCollection AddTransmitters(
+            this IServiceCollection serviceCollection, 
+            IConfiguration configuration
+            )
+        {
+            var groupNames = new List<string>();
+            configuration.Bind("GroupNames", groupNames);
+            
+            return serviceCollection
+                .AddTransient<IPostTransmitter, PostTransmitter>()
+                .AddTransient<IGroupTransmitter>(s => 
+                    new GroupTransmitter(
+                        s.GetService<VkApiFactory>(),
+                        groupNames
+                    )
+                );
+        }
+    }
+}
