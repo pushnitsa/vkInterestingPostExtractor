c5ff1eb177380735bee6138b5dac510304810a7a
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-03-11 22:48:24 +0200

    Add api factory extension

---
+++ VkInterestingPostExtractor/Extensions/AddVkApiFactoryExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using VkInterestingPostExtractor;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    internal static class AddVkApiFactoryExtension
+    {
+        public static IServiceCollection AddVkApiFactory(
+            this IServiceCollection serviceCollection,
+            IConfiguration configuration)
+        {
+            return serviceCollection
+                .AddTransient(s => new VkApiFactory(
+                    user: configuration.GetValue<string>("User"),
+                    password: configuration.GetValue<string>("Password"),
+                    applicationId: configuration.GetValue<ulong>("ApplicationId")
+                ));
+        }
+    }
+}
