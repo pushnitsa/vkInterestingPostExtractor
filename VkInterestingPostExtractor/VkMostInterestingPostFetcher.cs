e014d277ff8def89cb0278fd305f5050a022551a
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-03-11 23:02:36 +0200

    Add post handler to fetcher

--- VkInterestingPostExtractor/VkMostInterestingPostFetcher.cs
+++ VkInterestingPostExtractor/VkMostInterestingPostFetcher.cs
@@ -7,23 +7,27 @@ namespace VkInterestingPostExtractor
     {
         private readonly GroupInfoCollection _groupInfoCollection;
         private readonly WallPostFetcher _wallPostFetcher;
+        private readonly PostHandler _postHandler;
 
         public VkMostInterestingPostFetcher(
             GroupInfoCollection groupInfoCollection,
-            WallPostFetcher wallPostFetcher
+            WallPostFetcher wallPostFetcher,
+            PostHandler postHandler
             )
         {
             _groupInfoCollection = groupInfoCollection;
             _wallPostFetcher = wallPostFetcher;
+            _postHandler = postHandler;
         }
 
         public void Fetch()
         {
-            var groupIds = _groupInfoCollection.GetGroupIds();
+            var groupIfoCollection = _groupInfoCollection.GetGroupInfoCollection();
 
-            foreach (var groupId in groupIds)
+            foreach (var groupInfo in groupIfoCollection)
             {
-                var posts = _wallPostFetcher.FetchPosts(groupId);
+                var posts = _wallPostFetcher.FetchPosts(groupInfo.GroupId);
+                _postHandler.Handle(groupInfo.MembersCount, posts);
             }
         }
     }
